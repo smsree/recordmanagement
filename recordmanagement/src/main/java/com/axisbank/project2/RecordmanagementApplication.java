f4d93f203beaef9ce3d6b8dc3708ea5da9ffd3bd
smsree <103555247+smsree@users.noreply.github.com>
2022-07-27 15:29:59 +0530

    Add files via upload

--- recordmanagement/src/main/java/com/axisbank/project2/RecordmanagementApplication.java
+++ recordmanagement/src/main/java/com/axisbank/project2/RecordmanagementApplication.java
@@ -1,14 +1,21 @@
 package com.axisbank.project2;
 
+import javax.annotation.Resource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.axisbank.project2.service.FilesStorageService;
+
 @SpringBootApplication
 public class RecordmanagementApplication {
 
+	@Resource
+	 FilesStorageService storageService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RecordmanagementApplication.class, args);
 	}
@@ -17,5 +24,11 @@ public class RecordmanagementApplication {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+
+	  public void run(String... arg) throws Exception {
+	    storageService.deleteAll();
+	    storageService.init();
+	  }
 
 }
